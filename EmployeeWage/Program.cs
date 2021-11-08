1ee357d8913d671cd6b86fda781215615baf6c36
Harshit <yoharshit98@gmail.com>
2021-11-09 02:55:22 +0530

    [Harshit Added Wages for a Month]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,37 +1,39 @@
 ﻿using System;
 
-namespace UC_4UsingSwitchingCaseEmpWage
+namespace UC_5CalculatingWagesFor_Month
 {
     class Program
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_WAGE_PER_HOUR = 20;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            //Variable
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
+            //vARIABLE
+            int empHrs = 0, empwage = 0, totalempwage = 0;
             //Computation
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empwage = empHrs * EMP_RATE_PER_HOUR;
+                totalempwage += empwage;
+                Console.WriteLine("Emp Wage : " + empwage);
             }
-            empWage = empHrs * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
-
-
+            Console.WriteLine("Total Emp Wage : " + totalempwage);
         }
     }
 }
