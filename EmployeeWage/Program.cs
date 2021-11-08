78a980ed1bab0aa5c33e0155c4114d2271b594a7
Harshit <yoharshit98@gmail.com>
2021-11-09 02:14:00 +0530

    [Harshit Random EmployeeAttendance]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace EmployeeAttendance
 {
     class Program
     {
         static void Main(string[] args)
         {
-               //master branch 
-            Console.WriteLine("Welcome To Employee Wage Computation Program");
-            Console.ReadLine();
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
