111450c2f8ec5271e8f56ec769f6bc059145ec40
Harshit <yoharshit98@gmail.com>
2021-11-09 02:50:20 +0530

    [Harshit Added SwitchCase]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,35 +1,37 @@
 ﻿using System;
 
-namespace UC_3PartTimeOr_FullTimeWages
+namespace UC_4UsingSwitchingCaseEmpWage
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_WAGE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            //Constatnts
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_WAGE_PER_HOUR = 20;
             //Variable
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
+
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Emp wage : " + empWage);
+            Console.WriteLine("Emp Wage : " + empWage);
+
+
         }
     }
 }
