2fa6f9efaf0705f535af5af431214117ba3fa7c3
Harshit <yoharshit98@gmail.com>
2021-11-09 01:27:18 +0530

    [Harshit EmployeeWage Computation Program]

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               //master branch 
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            Console.ReadLine();
+        }
+    }
+}
