b3f5289277d39988f1b350520b81719cdcbb8d80
Harshit <yoharshit98@gmail.com>
2021-11-09 02:45:48 +0530

    [Harshit Added PartTime Employee]

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 
-namespace CalculateEmployeeWage
+namespace UC_3PartTimeOr_FullTimeWages
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Constants
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            //Constatnts
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_WAGE_PER_HOUR = 20;
             //Variable
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
@@ -23,9 +28,8 @@ namespace CalculateEmployeeWage
             {
                 empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            empWage = empHrs * EMP_WAGE_PER_HOUR;
+            Console.WriteLine("Emp wage : " + empWage);
         }
-
     }
 }
